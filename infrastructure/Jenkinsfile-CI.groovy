f6a4dabd432bb44832872225971e38e237558170
bertolla <p.bertolla@noi.bz.it>
2020-10-23 12:23:16 +0200

    add env file for CI

--- infrastructure/Jenkinsfile-CI.groovy
+++ infrastructure/Jenkinsfile-CI.groovy
@@ -1,20 +1,28 @@
 pipeline {
     agent {
         dockerfile {
-            filename 'docker/Dockerfile'
+            filename 'infrastructure/docker/Dockerfile'
             additionalBuildArgs '--build-arg JENKINS_USER_ID=`id -u jenkins` --build-arg JENKINS_GROUP_ID=`id -g jenkins`'
         }
     }
-
+    environment{
+        CLIENT_SECRET=credentials('webcomp-mobility-traffic_client-secret')
+    }
     stages {
+        stage('Configure'){
+            steps{
+                sh "echo 'CLIENT_SECRET=${CLIENT_SECRET}'>> .env"
+                sh "echo 'CLIENT_ID=it.bz.opendatahub.webcomponents.mobility-traffic' >>.env"
+                sh """echo 'TOKEN_URL="https://auth.opendatahub.bz.it/auth/realms/noi/protocol/openid-connect/token' >>.env"""
+            }
+        }
         stage('Dependencies') {
             steps {
-                sh 'npm install'
+                sh 'npm ci'
             }
         }
         stage('Test') {
             steps {
-                sh 'npm run lint'
                 sh 'npm run test'
             }
         }
