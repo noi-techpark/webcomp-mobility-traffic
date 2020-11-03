f053c1c17445985803c9b707c4bbe15908f3bdba
Peter Moser <p.moser@noi.bz.it>
2020-11-03 14:37:08 +0100

    infra: Use new CDN build

--- infrastructure/Jenkinsfile-Release.groovy
+++ infrastructure/Jenkinsfile-Release.groovy
@@ -37,7 +37,7 @@ pipeline {
         }
         stage('Build') {
             steps {
-                sh 'npm run build'
+                sh 'npm run build:cdn'
             }
         }
         stage('Git Tag') {
